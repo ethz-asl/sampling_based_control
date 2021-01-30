0c1c82235c534d126af0f2831d01142bf1e60b1d
Giuseppe Rizzi <grizzi@mavt.ethz.ch>
2021-01-30 11:23:30 +0100

    manipulaiton_msgs package

---
+++ mppi_examples/manipulation_msgs/include/manipulation_msgs/conversions.h
@@ -0,0 +1,34 @@
+//
+// Created by giuseppe on 30.01.21.
+//
+
+#pragma once
+#include "manipulation_msgs/State.h"
+
+#include <Eigen/Core>
+
+namespace manipulation::conversions {
+
+void msgToEigen(const manipulation_msgs::State& stateRos, Eigen::VectorXd& state);
+
+void eigenToMsg(const Eigen::VectorXd& state, manipulation_msgs::State&);
+
+void toEigenState(const Eigen::Vector3d& base_pose, const Eigen::Vector3d& base_twist,
+                  const Eigen::VectorXd& arm_position, const Eigen::VectorXd& arm_velocity,
+                  const double& object_position, const double& object_velocity,
+                  const bool& contact_state, Eigen::VectorXd& x);
+
+void toMsg(const Eigen::Vector3d& base_pose, const Eigen::Vector3d& base_twist,
+           const Eigen::VectorXd& arm_position, const Eigen::VectorXd& arm_velocity,
+           const double& object_position, const double& object_velocity,
+           const bool& contact_state, manipulation_msgs::State );
+
+void toEigenState(const Eigen::VectorXd& arm_position, const Eigen::VectorXd& arm_velocity,
+                  const double& object_position, const double& object_velocity,
+                  const bool& contact_state, Eigen::VectorXd& x);
+
+void toMsg(const Eigen::VectorXd& arm_position, const Eigen::VectorXd& arm_velocity,
+           const double& object_position, const double& object_velocity,
+           const bool& contact_state, manipulation_msgs::State );
+
+}  // namespace manipulation::conversions
