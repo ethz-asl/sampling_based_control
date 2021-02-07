bcec99ec8f75a60d7f2e6a245743d37a5bcd045f
Giuseppe Rizzi <grizzi@mavt.ethz.ch>
2021-02-07 16:21:40 +0100

    more changes

--- mppi_ros/scripts/plot_experiment.py
+++ mppi_ros/scripts/plot_experiment.py
@@ -4,13 +4,17 @@ import rosbag
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = {'reset_time': [],
-        'rollouts_cost': [],
-        'weights': [],
-        'optimal_rollout_cost': []}
+
+# Hypothesis: a very large number of particles is ineffective in terms of quality solution
+# and can even be detrimental in terms of performance:
+# x: time
+# y1: mean cost sample size 1
+# y2: mean cost sample size 2
+# ...
+# yn: mean cost sample size n
 
 
-def extract_data(bag_file):
+def extract_data(data, bag):
     for topic, msg, t in bag.read_messages(topics=['/mppi_data']):
         if len(msg.weights.array) == 0:
             continue
@@ -39,30 +43,57 @@ def plot_samples(ax, data):
     ax.legend()
 
 
-def plot_cost(ax, data):
+def plot_cost(ax, data, prefix='', fill=True):
     t = np.arange(len(data['rollouts_cost']))
     costs = np.asarray(data['rollouts_cost'])
+    samples = costs.shape[1]
     optimal_rollout_cost = np.asarray(data['optimal_rollout_cost'])
     mean_costs = costs.mean(axis=1)
     std_costs = costs.std(axis=1)
     ax.set_title("Cost")
-    ax.plot(t, mean_costs, lw=2, label='mean cost', color='red')
-    ax.fill_between(t, mean_costs + std_costs, mean_costs - std_costs, facecolor='orange', alpha=0.5, label='$\sigma$ range')
+    ax.plot(t, mean_costs, lw=2, label=prefix + 'mean cost, samples# = ' + str(samples))
+    if fill:
+        ax.fill_between(t, mean_costs + std_costs, mean_costs - std_costs, facecolor='orange', alpha=0.5,
+                        label='$\sigma$ range')
     ax.grid()
     ax.legend()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot data from experiment.')
-    parser.add_argument('--bag-file', type=str, help='path to the bag file containing experiment data.')
+    parser.add_argument('--bag-dir', type=str, help='path to the bag file containing experiment data.')
     args = parser.parse_args()
 
-    print("Reading from: {}".format(args.bag_file))
-    bag = rosbag.Bag(args.bag_file)
+    import os
+    import sys
+    import glob
 
-    extract_data(bag)
-    fig, ax = plt.subplots(2)
-    plot_samples(ax[0], data)
-    plot_cost(ax[1], data)
+    bag_files = glob.glob(os.path.join(args.bag_dir, '*.bag'))
+    print("Found {} bags in {}".format(len(bag_files), args.bag_dir))
+    if len(bag_files) == 0:
+        sys.exit(0)
+
+    comparison_fig = plt.figure()
+    comparison_ax = comparison_fig.add_subplot(111)
+
+    for idx, bag_file in enumerate(bag_files):
+        print("Opening: {}".format(bag_file))
+        bag = rosbag.Bag(bag_file)
+
+        d = {'reset_time': [],
+             'rollouts_cost': [],
+             'weights': [],
+             'optimal_rollout_cost': []}
+
+        extract_data(d, bag)
+
+        fig = plt.figure()
+
+        ax1 = fig.add_subplot(2, 1, 1)
+        ax2 = fig.add_subplot(2, 1, 2)
+
+        plot_samples(ax1, d)
+        plot_cost(ax2, d)
+        plot_cost(comparison_ax, d, fill=False, prefix=str(idx) + '_')
 
     plt.show()
