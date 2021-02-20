b68812ce13a7cdd33aed02d7d3599153ff3790d2
aslpanda <ethz.asl.panda@gmail.com>
2021-02-20 12:14:29 +0100

    demo script

---
+++ mppi_examples/mppi_manipulation/scripts/demo.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env  python
+
+import rospy
+from std_msgs.msg import Int64
+
+
+if __name__ == "__main__":
+	rospy.init_node("demo_node")
+	rospy.loginfo("Starting the demo in 3 seconds.")
+	rospy.sleep(3.0)
+
+	mode_publisher = rospy.Publisher("/mode", Int64, queue_size=10)
+	
+	ans = input("Have you selected the default pose from the marker [y/n]")
+	if ans != 'y':
+		rospy.loginfo("Exiting")
+		return
+
+	go_to_handle_mode = Int64()
+	go_to_handle_mode.data = 1
+
+	open_mode = Int64()
+	open_mode.data = 2
+
+	default_mode = Int64()
+	default_mode.data = 0
+
+
+	mode_publisher.publish(go_to_handle_mode)
+	rospy.loginfo("Reaching the handle!")
+	rospy.sleep(5.0)
+
+	mode_publisher.publish(open_mode)
+	rospy.loginfo("Opening the door!")
+	rospy.sleep(5.0)
+	
+	mode_publisher.publish(default_mode)
+	rospy.loginfo("Going back to default pose!")
+	rospy.sleep(5.0)
+
+	rospy.loginfo("Done :)")
+	
+
