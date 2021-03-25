a9b96f4a07197525af17f31d67474055e31acf54
kirandoshi <doshi@student.ethz.ch>
2021-03-25 13:41:56 +0100

    update to given data structure and add main to test

--- policy_learning/scripts/learning.py
+++ policy_learning/scripts/learning.py
@@ -16,14 +16,21 @@ class StateActionDataset(Dataset):
         self.root_dir = root_dir
         self.transform = transform
 
-        # root_dir must have a state data file and an action data file
+        # root_dir must have a data file
         # TODO: (Kiran) adapt to final i/o of data collection
 
 
-        state_file = os.path.join(root_dir, 'states.csv')
-        action_file = os.path.join(root_dir, 'actions.csv')
-        self.state = pd.read_csv(state_file, header=None)
-        self.action = pd.read_csv(action_file, header=None)
+        file_name = os.path.join(root_dir, 'data', 'out.csv')
+        # code to read in following csv format:
+        # n_states, n_actions, state_values, action_values
+        n_states = pd.read_csv(file_name, header=None, usecols=[0])  # assusmes that n_states does not change in one csv file
+        n_actions = pd.read_csv(file_name, header=None, usecols=[1]) # same as above for n_actions
+        n_states = n_states.values[0][0]
+        n_actions = n_actions.values[0][0]
+        self.state = pd.read_csv(file_name, header=None,
+            usecols=range(2, n_states+2))
+        self.action = pd.read_csv(file_name, header=None,
+            usecols=range(n_states+2, n_states+n_actions+1)) # last entry should be +2 but csv is one column too short for some reason
 
     def __len__(self):
         """
@@ -41,12 +48,17 @@ class StateActionDataset(Dataset):
         """
         Override base class method.
         """
-        state = self.state[idx]
-        action = self.action[idx]
+        state = self.state.values[idx]
+        action = self.action.values[idx]
         sample = {'state': state, 'action': action}
         return sample
 
 
 if __name__ == "__main__":
-    #task_path = os.path.dirname(os.path.realpath(__file__))
-    print("hello world")
+    task_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = task_path + "/../"
+    dataset = StateActionDataset(root_dir=file_path)
+    idx=0
+    sample = dataset[idx]
+    print("Index: ", idx, "\nstate: " , sample['state'], "\naction: ", sample['action'])
+    print(len(dataset))
