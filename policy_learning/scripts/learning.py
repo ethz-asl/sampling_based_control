f364476612221416cf357a3b5cc5789639822532
kirandoshi <doshi@student.ethz.ch>
2021-03-27 10:49:03 +0100

    added class to handle learning loop - basic structure done

--- policy_learning/scripts/learning.py
+++ policy_learning/scripts/learning.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 import torch.nn as nn
 
 class StateActionDataset(Dataset):
@@ -61,6 +62,7 @@ class StateActionDataset(Dataset):
             int, int: dimensions of the state and action spaces
         """
         return self._n_states, self._n_actions
+        
 
 class MLPSampler(nn.Module):
     """
@@ -77,11 +79,80 @@ class MLPSampler(nn.Module):
             nn.Linear(512,n_out)
         )
     def forward(self, x):
+        """
+        return: output of model
+        """
         return self.linear_relu_stack(x)
 
 
+class PolicyLearner:
+    """
+    Class to handle the learning loop and give back an action after net has trained.
+    """
+
+    def __init__(self, file_path, device):
+        """
+        Args:
+            file_path (string): directory with data folder
+            device (string): choose gpu or cpu
+        """
+        # load data set
+        self.dataset = StateActionDataset(root_dir=file_path)
+        self.n_states, self.n_actions = self.dataset.get_dimensions()
+        # initialise MLP model
+        self.model = MLPSampler(self.n_states, self.n_actions-1).to(device) #reduce action size for now because of csv n_action bug
+        self._is_trained = False
+
+    def train(self):
+        """
+        Main training loop.
+        """
+
+        # Training parameters
+        epochs = 2
+        batch_size = 2
+        learning_rate = 1e-3
+
+        # initialise loss function
+        loss_fn = nn.MSELoss()
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=learning_rate)
+
+        # initialise data loader
+        train_dataloader = DataLoader(self.dataset, batch_size=batch_size)
+        size = len(train_dataloader.dataset)
+
+        for t in range(epochs):
+            print(f"Epoch {t+1}\n-------------------------------")
+            for batch, sample in enumerate(train_dataloader):
+                pred = self.model(sample['state'].float())
+                loss = loss_fn(pred, sample['action'].float())
+
+                # Backpropagation
+                optimizer.zero_grad() # set grad to zero firs
+                loss.backward()
+                optimizer.step()
+
+                if batch % 10 == 0:
+                    loss, current = loss.item(), batch * len(sample['state'])
+                    print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+        self._is_trained = True
+
+    def get_action(self, state):
+        """
+        returns action from trained model
+        """
+        if self._is_trained:
+            with torch.no_grad():
+                return self.model(torch.as_tensor(state, dtype=torch.float32))
+        else:
+            print('Model has not been trained yet. Action is the output of random initialised network')
+            with torch.no_grad():
+                return self.model(torch.as_tensor(state, dtype=torch.float32))
+
+
 if __name__ == "__main__":
-    from torch.utils.data import DataLoader
+
 
     task_path = os.path.dirname(os.path.realpath(__file__))
     file_path = task_path + "/../"
@@ -96,13 +167,18 @@ if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = MLPSampler(n_states, n_actions).to(device)
     #print(model)
+    learner = PolicyLearner(file_path, device)
+    learner.train()
 
-    # make data in correct format for torch
-    train_dataloader = DataLoader(dataset, batch_size=1)
+    # # make data in correct format for torch
+    # train_dataloader = DataLoader(dataset, batch_size=1)
+    #
+    # for different_sample in train_dataloader:
+    #
+    #     #print(different_sample)
+    #     state = different_sample['state']
+    #     a_pred = model(state.float())
+    #     #print(a_pred)
 
-    for different_sample in train_dataloader:
-
-        #print(different_sample)
-        state = different_sample['state']
-        a_pred = model(state.float())
-        #print(a_pred)
+    action = learner.get_action(sample['state'])
+    print(action)
