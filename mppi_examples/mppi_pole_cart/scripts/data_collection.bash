7f71373aea5826fff71c0c0fd24327a461ebcce2
kirandoshi <doshi@student.ethz.ch>
2021-04-07 09:10:47 +0200

    add variables

--- mppi_examples/mppi_pole_cart/scripts/data_collection.bash
+++ mppi_examples/mppi_pole_cart/scripts/data_collection.bash
@@ -2,4 +2,11 @@
 
 output_path="/home/kiran/test_file_new.hdf5"
 x_position=2.0
-roslaunch mppi_pole_cart pole_cart_data_collection.launch learner_output_path:=$output_path x:=${x_position}
+x_dot=0
+PI=3.14
+theta=${PI}
+theta_dot=0
+roslaunch mppi_pole_cart pole_cart_data_collection.launch \
+learner_output_path:=$output_path \
+x:=${x_position} x_d:=${x_dot} \
+theta:=${theta} theta_d:=${theta_dot}
