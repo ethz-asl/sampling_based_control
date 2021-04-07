f4c76cc707eece04add2a9905cca957f97664977
kirandoshi <doshi@student.ethz.ch>
2021-04-07 14:11:57 +0200

    add logging of values and bugfix

--- mppi_examples/mppi_pole_cart/scripts/data_collection.bash
+++ mppi_examples/mppi_pole_cart/scripts/data_collection.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-dir is relative and should work on any machine as long as file structure of
-package remains the same
+# dir is relative and should work on any machine as long as file structure of
+# package remains the same
 data_dir="../../../policy_learning/data/"
 cd $data_dir
 current_time=$(date +%m%d%H%M%S)
@@ -10,10 +10,12 @@ cd $current_time
 current_dir=$(pwd)
 # loop over random inital conditions, for each initial condtition save a new
 # data file
+echo "x_position,x_dot,theta,theta_dot" >> value_log.csv
 for ((i=0; i<10; i++))
 do
   output_file="/run_${i}.hdf5"
   output_path=$current_dir$output_file
+
   # The random variable changed with the modulo does not actually yield proper
   # uniform distribution, only an approximation where the tail end of the numbers
   # are less likely (i.e. higher numbers are marginally less likely then smaller ones)
@@ -22,10 +24,12 @@ do
   PI=3.14
   theta=$(echo "scale=2; (-1^$(($RANDOM%2)))*($(($RANDOM%300))/100)" |bc)
   theta_dot=$(echo "scale=2; (-1^$(($RANDOM%2)))*($(($RANDOM%5))/10)" |bc)
-  # echo "x: "$x_position
-  # echo "x_d: "$x_dot
-  # echo "theta: "$theta
-  # echo "theta_d: "$theta_dot
+
+  # log the random values for reproducibility
+  echo $x_position","$x_dot","$theta","$theta_dot >> value_log.csv
+
+  # starts the simulation with the data collection option and the specified
+  # initial condition
   roslaunch mppi_pole_cart pole_cart_data_collection.launch \
   learner_output_path:=$output_path \
   x:=${x_position} x_d:=${x_dot} \
