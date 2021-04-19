861c06f479ee355b44b4cf6f5bb06a64cb9a1cb3
Andreas Voigt <voigta@ethz.ch>
2021-04-19 13:36:34 +0200

    Add option to reuse initial conditions from another dataset

--- mppi_examples/mppi_pole_cart/scripts/data_collection.bash
+++ mppi_examples/mppi_pole_cart/scripts/data_collection.bash
@@ -1,38 +1,62 @@
 #!/usr/bin/env bash
 
+while getopts c:n: opts; do
+   case ${opts} in
+      c) CVS_PATH=${OPTARG} ;;
+      n) USER_NAME=${OPTARG} ;;
+   esac
+done
+
 # dir is relative and should work on any machine as long as file structure of
 # package remains the same
-data_dir="../../../policy_learning/data/"
+script_dir="$(dirname "$0")"
+data_dir="$script_dir/../../../policy_learning/data/"
 cd $data_dir
-current_time=$(date +%m%d%H%M%S)
-mkdir $current_time
-cd $current_time
+dataset_name=$USER_NAME"_"$(date +%m%d%H%M%S)
+mkdir $dataset_name
+cd $dataset_name
 current_dir=$(pwd)
-# loop over random inital conditions, for each initial condtition save a new
-# data file
-echo "x_position,x_dot,theta,theta_dot" >> value_log.csv
-n_runs=50
-for ((i=0; i<$n_runs; i++))
+
+if [ -z "$CVS_PATH" ]
+  then
+    echo "No data csv provided. Generate own random data."
+
+      # loop over random inital conditions and save them to csv
+    echo "x_position,x_dot,theta,theta_dot" >> value_log.csv
+    n_runs=50
+    for ((i=0; i<$n_runs; i++))
+    do
+      # The random variable changed with the modulo does not actually yield proper
+      # uniform distribution, only an approximation where the tail end of the numbers
+      # are less likely (i.e. higher numbers are marginally less likely then smaller ones)
+      x_position=$(echo "scale=2; (-1^$(($RANDOM%2)))*($(($RANDOM%20))/10)" |bc)
+      x_dot=$(echo "scale=2; (-1^$(($RANDOM%2)))*($(($RANDOM%5))/10)" |bc)
+      PI=3.14
+      theta=$(echo "scale=2; (-1^$(($RANDOM%2)))*($(($RANDOM%300))/100)" |bc)
+      theta_dot=$(echo "scale=2; (-1^$(($RANDOM%2)))*($(($RANDOM%5))/10)" |bc)
+
+      # log the random values for reproducibility
+      echo $x_position","$x_dot","$theta","$theta_dot >> value_log.csv
+    done
+    echo "Data written to "$current_dir"/value_log.csv"
+  else
+    cp $CVS_PATH value_log.csv
+fi
+
+i=0
+while IFS="," read -r x_position x_dot theta theta_dot
 do
   output_file="/run_${i}.hdf5"
   output_path=$current_dir$output_file
-
-  # The random variable changed with the modulo does not actually yield proper
-  # uniform distribution, only an approximation where the tail end of the numbers
-  # are less likely (i.e. higher numbers are marginally less likely then smaller ones)
-  x_position=$(echo "scale=2; (-1^$(($RANDOM%2)))*($(($RANDOM%20))/10)" |bc)
-  x_dot=$(echo "scale=2; (-1^$(($RANDOM%2)))*($(($RANDOM%5))/10)" |bc)
-  PI=3.14
-  theta=$(echo "scale=2; (-1^$(($RANDOM%2)))*($(($RANDOM%300))/100)" |bc)
-  theta_dot=$(echo "scale=2; (-1^$(($RANDOM%2)))*($(($RANDOM%5))/10)" |bc)
-
-  # log the random values for reproducibility
-  echo $x_position","$x_dot","$theta","$theta_dot >> value_log.csv
-
   # starts the simulation with the data collection option and the specified
   # initial condition
   roslaunch mppi_pole_cart pole_cart_data_collection.launch \
   learner_output_path:=$output_path \
   x:=${x_position} x_d:=${x_dot} \
   theta:=${theta} theta_d:=${theta_dot}
-done
+
+  let "i+=1" 
+done < <(tail -n +2 value_log.csv)
+
+
+
