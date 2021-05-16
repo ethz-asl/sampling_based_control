5cda36aafb03dca92b01d8e32b2d8c4cb96d8340
Andreas Voigt <voigta@ethz.ch>
2021-05-16 16:50:29 +0200

    Fix exception handling

--- policy_learning/src/panda_expert.cpp
+++ policy_learning/src/panda_expert.cpp
@@ -18,12 +18,13 @@ class PandaExpert::Impl{
 PandaExpert::PandaExpert(std::unique_ptr<Policy> policy, 
                          std::unique_ptr<Dataset> dataset, 
                          const std::string& robot_description):
-  LearnedExpert(4, 1),
+  LearnedExpert(7, 7),
   pImpl(std::make_unique<Impl>()),
   dataset_(std::move(dataset)),
   policy_(std::move(policy))
 {
   pImpl->robot_model_.init_from_xml(robot_description);
+  if (policy_) policy_->check_sizes(13, 7);
 }
 
 PandaExpert::~PandaExpert() = default;
@@ -54,6 +55,10 @@ bool PandaExpert::collect_data(){
 void const PandaExpert::augment_observation(observation_t const& x, 
                                             augmented_observation_t& x_aug){
   pImpl->robot_model_.update_state(x.head<7>());
+
+  if (timed_ref_.rr.size() == 0){
+    throw std::runtime_error("PandaExpert: No reference set!");
+  }
 
   Eigen::Vector3d ref_t = timed_ref_.rr[0].head<3>();
   Eigen::Quaterniond ref_q(timed_ref_.rr[0].segment<4>(3));
