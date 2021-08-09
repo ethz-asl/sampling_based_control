a1e3942f252285e548e6cb78a31ea3de52e7dfcb
Giuseppe Rizzi <grizzi@mavt.ethz.ch>
2021-08-09 10:28:54 +0200

    working reference scheduler

--- mppi_examples/mppi_manipulation/include/mppi_manipulation/reference_scheduler.h
+++ mppi_examples/mppi_manipulation/include/mppi_manipulation/reference_scheduler.h
@@ -13,72 +13,13 @@ namespace manipulation {
 
 class ReferenceScheduler {
  public:
-  void add_reference(const double t, const mppi::reference_t& ref) {
-    if (!times_.empty() && t < times_.back()) {
-      std::cout
-          << "[ReferenceScheduler]: trying to add a reference back in the past."
-          << std::endl;
-      return;
-    }
-    mppi::reference_trajectory_t rt;
-    rt.rr.push_back(ref);
-    rt.tt.push_back(t);
-    schedule_.push_back(rt);
-    times_.push_back(t);
-    std::stringstream ss;
-    ss << "[ReferenceScheduler]: adding new reference." << std::endl;
-    ss << "  ref  = [" << ref.transpose() << "]" << std::endl;
-    ss << "  time = " << t << std::endl;
-    std::cout << ss.str();
-  }
-
-  bool has_reference(const double t) {
-    return times_.empty() ? false : t > times_[0];
-  }
-
-  bool parse_from_file(const std::string& file_path) {
-    std::cout << "[ReferenceScheduler]: parsing references from " << file_path
-              << std::endl;
-    YAML::Node config;
-    try {
-      config = YAML::LoadFile(file_path);
-    } catch (const YAML::ParserException& ex) {
-      std::cout << ex.what() << std::endl;
-    } catch (const YAML::BadFile& ex) {
-      std::cout << ex.what() << std::endl;
-    }
-
-    if (!config.IsSequence()) {
-      std::cout << "[ReferenceScheduler] Failed to parse references file."
-                << std::endl;
-      return false;
-    }
-
-    for (const auto& element : config) {
-      if (!element.IsMap()) {
-        std::cout << "[ReferenceScheduler]: element should be dictionary"
-                  << std::endl;
-      }
-      auto r = element["r"].as<std::vector<double>>();
-      auto t = element["t"].as<double>();
-      mppi::reference_t r_eigen;
-      r_eigen = mppi::reference_t::Zero(r.size());
-      r_eigen.setZero(r.size());
-      for (int i = 0; i < r.size(); i++) r_eigen(i) = r[i];
-      add_reference(t, r_eigen);
-    }
-    return true;
-  }
-
-  void set_reference(const double t, mppi::reference_trajectory_t& rt) {
-    if (t > times_[0]) {
-      auto lower = std::lower_bound(times_.begin(), times_.end(), t);
-      size_t index = std::distance(times_.begin(), lower);
-      rt = schedule_[index - 1];
-    }
-  }
+  void add_reference(const double t, const mppi::reference_t& ref);
+  bool has_reference(const double t);
+  bool parse_from_file(const std::string& file_path);
+  void set_reference(const double t, mppi::reference_trajectory_t& rt);
 
  private:
+  int reference_counter_ = 0;
   std::vector<double> times_;
   std::vector<mppi::reference_trajectory_t> schedule_;
 };
