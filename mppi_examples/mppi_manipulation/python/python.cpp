d1ee6166fa83529f5e4e70bea5d687af4a2f0452
gis <giuschio98@gmail.com>
2021-11-04 14:44:07 +0100

    init pybind setup

---
+++ mppi_examples/mppi_manipulation/python/python.cpp
@@ -0,0 +1,16 @@
+/*
+ * Created by Giulio Schiavi
+ * 4 NOV 2021
+ * Generates a shared library file at catkin_ws/devel/.private/mppi_manipulation/lib/pymppi_manipulation.cpython-38-x86_64-linux-gnu.so
+ */
+
+
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <pybind11/iostream.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pymppi_manipulation, m) {
+}
