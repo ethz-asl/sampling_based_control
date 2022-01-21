a40aca88fcdbd3c0068e3d8f5286d468b976377b
BoySun045 <boyang-sun@outlook.com>
2022-01-21 11:07:56 +0100

    Sync with ASL PC

--- mppi_examples/object_modeling_playground/include/object_modeling_playground/mug.h
+++ mppi_examples/object_modeling_playground/include/object_modeling_playground/mug.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "object_modeling_playground/object.h"
+#include <manipulation_msgs/State.h>
+#include <manipulation_msgs/MugPrimitive.h>
+#include <Eigen/Dense>
 
 class Mug: public Object
 {
@@ -10,15 +13,38 @@ class Mug: public Object
     Mug(const ros::NodeHandle& nh);
     ~Mug() = default;
 
-    void update_obj_TF() override;
+    void update_TF() override;
     void pub_state() override;
     void update_kp_markers() override;
-    void fit_primitive() override;
-    void vis_primitive() override;
-    void pose_estimate() override;
-
+    void primitive_visualize() override;
+    void primitive_estimate() override;
+    void update() override;
+  
 
   private:
+    bool estimate_center_pose(Eigen::Vector3d& pos,
+                        Eigen::Vector4d& orien);
+
+    double point_to_line(const Eigen::Vector3d& pos_1,
+                        const Eigen::Vector3d& pos_2,
+                        const Eigen::Vector3d& pos_3);
+
+    Eigen::Matrix3d rot_of_two_frame(const Eigen::Matrix3d& ref_rot,
+                            const Eigen::Matrix3d& rel_rot);
+
     ros::NodeHandle nh_;
-    std::vector<int> set_nums;
+
+    manipulation_msgs::MugPrimitive mug_primitive;
+    double center_roll;
+    double center_pitch;
+    double center_yaw;
+    Eigen::Vector3d center_line;
+    std::vector<double> height;
+    std::vector<double> radius;
+
+    int bottom_idx;
+    int top_idx;
+    int handle_idx;
+    int avg_idx;
+
 };
--- mppi_examples/object_modeling_playground/include/object_modeling_playground/object.h
+++ mppi_examples/object_modeling_playground/include/object_modeling_playground/object.h
@@ -4,41 +4,46 @@
 #include <chrono>
 #include <Eigen/Core>
 #include <std_msgs/String.h>
+#include <std_msgs/Header.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/Pose.h>
 #include <std_msgs/Float64.h>
 #include <std_msgs/Float64MultiArray.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <interactive_markers/interactive_marker_server.h>
-
+#include <tf/transform_listener.h>
 
 class Object
 {
 public:
     Object(const ros::NodeHandle& nh);
     ~Object() = default;
-    virtual void update_obj_TF(){};
+    virtual void update_TF(){};
     virtual void pub_state(){};
     virtual void update_kp_markers(){};
-    virtual void fit_primitive(){};
-    virtual void vis_primitive(){};
-    virtual void pose_estimate(){};
-
+    virtual void primitive_visualize(){};
+    virtual void primitive_estimate(){};
+    virtual void update(){};
+    
     geometry_msgs::TransformStamped obj_trans;
+    geometry_msgs::TransformStamped prim_trans;
     geometry_msgs::TransformStamped kp_trans;
     
 protected:
     bool init_param();
     void kp_int_callback(const geometry_msgs::PoseArray::ConstPtr& msg);
-    void init_kp_TF(); // init keypoints frame TF
 
     bool sim;
     std::string kp_3d_topic;
 
     // obj config vars
     std::string ref_frame;
+    std::string obj_frame;
+    std::string prim_frame;
+
     std::string object_name;
     std::vector<double> obj_scale_;
     std::vector<double> obj_pos_;
@@ -47,7 +52,9 @@ protected:
 
     // kp vars
     int kp_num;
+    int gt_kp_num;
     Eigen::VectorXd keypoints_xd;
+    Eigen::VectorXd keypoints_sim_xd;
     std::vector<double> keypoints_;
     std::vector<double> keypoints_past_;
 
@@ -56,7 +63,7 @@ protected:
 
     ros::Publisher state_publisher_;
     sensor_msgs::JointState state_; 
-
+    
     ros::Publisher kp_publisher_;
     visualization_msgs::Marker kp_marker_;
     visualization_msgs::MarkerArray kp_markers_;
@@ -65,11 +72,10 @@ protected:
     visualization_msgs::MarkerArray primitive_markers_;
     visualization_msgs::Marker primitive_marker_;
 
+    tf2_ros::TransformBroadcaster broadcaster;
 
-
-    int bottom_ind;
-    std::vector<double> height;
-    std::vector<double> radius;
+    bool kp_msg_received = false;
+    ros::Time ros_time;
 
 private:
     // params of approx primitive
