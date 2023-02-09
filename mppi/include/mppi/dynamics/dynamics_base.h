ethz-asl/sampling_based_control

commit d6333264efef1193d451b5fa8f89dfdf91120b2a

fix: add conversion function from actual observation to extended state vector used in dynamics simulation
BREAKING CHANGE: breaks interface with other mppi examples

mppi/include/mppi/dynamics/dynamics_base.h
@@ -33,6 +33,15 @@ class DynamicsBase {
   virtual observation_t step(const input_t& u, const double dt) = 0;
   virtual const observation_t get_state() const = 0;
 
+  /**
+   * @brief Convert an actual observed state to the extended state vector used
+   * in the implementation specific dynamics simulation.
+   * @param x: current actual observation
+   * @return extended state vector
+   */
+  virtual observation_t get_extended_state_from_observation(
+      const observation_t& x) const = 0;
+
   // TODO(giuseppe): param x is never used!
   virtual input_t get_zero_input(const observation_t& x) {
     return input_t::Zero(this->get_input_dimension());

mppi/src/controller/mppi.cpp
@@ -256,7 +256,7 @@ void PathIntegral::set_observation(const observation_t& x, const double t) {
 
 void PathIntegral::copy_observation() {
   std::unique_lock<std::shared_mutex> lock(state_mutex_);
-  x0_internal_ = x0_;
+  x0_internal_ = dynamics_->get_extended_state_from_observation(x0_);
   t0_internal_ = reset_time_;
   hold_time_end_internal_ = hold_time_end_;
   lock.unlock();

mppi_examples/mppi_omav_interaction/include/mppi_omav_interaction/dynamics.h
@@ -93,6 +93,8 @@ class OMAVVelocityDynamics : public mppi::DynamicsBase {
   input_t get_zero_input(const observation_t &x) override;
 
   const observation_t get_state() const override { return x_; }
+  observation_t get_extended_state_from_observation(
+      const observation_t &x) const override;
 
   force_t get_contact_forces(double &unwanted_contact);
   force_t get_contact_forces() {

mppi_examples/mppi_omav_interaction/src/dynamics.cpp
@@ -98,9 +98,7 @@ mppi::DynamicsBase::observation_t OMAVVelocityDynamics::step(const input_t &u,
 }
 
 void OMAVVelocityDynamics::reset(const observation_t &x) {
-  // reset internal simulation state vector with real state vector
-  x_.head<omav_state_description::SIZE_OMAV_STATE>() = x;
-  x_(omav_state_description_simulation::UNWANTED_CONTACT) = 0.0;
+  x_ = x;
 
   // reset omav and object in raisim
   omav_->setState(
@@ -110,6 +108,16 @@ void OMAVVelocityDynamics::reset(const observation_t &x) {
   object_->setState(
       x_.segment<1>(omav_state_description_simulation::OBJECT_ORIENTATION),
       x_.segment<1>(omav_state_description_simulation::OBJECT_VELOCITY));
+}
+
+OMAVVelocityDynamics::observation_t
+OMAVVelocityDynamics::get_extended_state_from_observation(
+    const observation_t &x) const {
+  observation_t extended_state(static_cast<int>(
+      omav_state_description_simulation::SIZE_OMAV_STATE_SIMULATION));
+  extended_state.setZero();
+  extended_state.head<omav_state_description::SIZE_OMAV_STATE>() = x;
+  return extended_state;
 }
 
 mppi::DynamicsBase::input_t OMAVVelocityDynamics::get_zero_input(

commit aad720c61fc1ff6aa28f601c551235b8f9ce5a53

fix: add basic copy implementation for extended state vector conversion to dynamics base class
fix for breaking change introduced in d633326

mppi/include/mppi/dynamics/dynamics_base.h
@@ -35,12 +35,15 @@ class DynamicsBase {
 
   /**
    * @brief Convert an actual observed state to the extended state vector used
-   * in the implementation specific dynamics simulation.
+   * in the implementation specific dynamics simulation. Needs to be implemented
+   * accordingly in the derived dynamics class if these two vectors differ.
    * @param x: current actual observation
-   * @return extended state vector
+   * @return extended state vector used in the dynamics simulation
    */
   virtual observation_t get_extended_state_from_observation(
-      const observation_t& x) const = 0;
+      const observation_t& x) const {
+    return x;
+  }
 
   // TODO(giuseppe): param x is never used!
   virtual input_t get_zero_input(const observation_t& x) {