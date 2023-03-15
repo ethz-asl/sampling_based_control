598886bd77d37ca4f7c951b7aed3796e2ca3800a
Michael Schmid <schmimi@student.ethz.ch>
2023-03-15 11:23:00 +0100

    feat: use gram schmidt before object yaw angle filtering

--- mppi_examples/mppi_omav_interaction/src/nodes/object_pose_filter_node.cpp
+++ mppi_examples/mppi_omav_interaction/src/nodes/object_pose_filter_node.cpp
@@ -13,6 +13,20 @@
 
 #include "mav_msgs/common.h"
 
+struct FilterParameters {
+  struct Coefficients {
+    double position;
+    double orientation;
+  } coefficients;
+
+  struct Thresholds {
+    double position;
+    double orientation;
+  } thresholds;
+
+  double timeout;
+};
+
 class ObjectPoseFilter {
  public:
   ObjectPoseFilter(ros::NodeHandle nh, ros::NodeHandle private_nh)
@@ -21,11 +35,22 @@ class ObjectPoseFilter {
         private_nh_(private_nh),
         tf2_filter_(object_pose_subscriber_, tf2_buffer_, "", 10, 0),
         filtered_position_(Eigen::Vector3d::Zero()),
-        filtered_yaw_(0.0) {
+        filtered_yaw_(0.0),
+        filter_initialized_(false) {
     std::string object_pose_topic;
     private_nh_.getParam("target_frame", target_frame_);
     private_nh_.getParam("object_pose_topic", object_pose_topic);
-    private_nh_.getParam("filter_factor", filter_factor_);
+
+    // get parameters
+    private_nh_.getParam("position_filter_coeff",
+                         filter_parameters_.coefficients.position);
+    private_nh_.getParam("orientation_filter_coeff",
+                         filter_parameters_.coefficients.orientation);
+    private_nh_.getParam("filter_timeout", filter_parameters_.timeout);
+    private_nh_.getParam("position_threshold",
+                         filter_parameters_.thresholds.position);
+    private_nh_.getParam("orientation_threshold",
+                         filter_parameters_.thresholds.orientation);
 
     object_pose_subscriber_.subscribe(nh_, object_pose_topic, 10);
     object_pose_publisher_ =
@@ -41,22 +66,65 @@ class ObjectPoseFilter {
     try {
       tf2_buffer_.transform(*pose_msg_ptr_in, pose_msg_transformed, target_frame_);
 
-      Eigen::Vector3d current_position = mav_msgs::vector3FromPointMsg(pose_msg_transformed.pose.position);
+      float position_coeff = filter_parameters_.coefficients.position;
+      float orientation_coeff = filter_parameters_.coefficients.orientation;
 
-      float factor = filter_factor_;
+      // reset filter
       ros::Duration dt = pose_msg_transformed.header.stamp - previous_timestamp_;
       previous_timestamp_ = pose_msg_transformed.header.stamp;
 
-      if (dt.toSec() > 0.5) {
-        factor = 1.0f;
+      if (dt.toSec() > filter_parameters_.timeout) {
+        position_coeff = 1.0f;
+        orientation_coeff = 1.0f;
       }
 
-      filtered_position_ = filter_factor_*current_position + (1.0f - filter_factor_) * filtered_position_;
+      // Position
+      Eigen::Vector3d current_position =
+          mav_msgs::vector3FromPointMsg(pose_msg_transformed.pose.position);
 
-      Eigen::Quaterniond current_orientation = mav_msgs::quaternionFromMsg(pose_msg_transformed.pose.orientation);
-      double current_yaw = mav_msgs::yawFromQuaternion(current_orientation);
-      filtered_yaw_ = filter_factor_*current_yaw + (1.0f - filter_factor_) * filtered_yaw_;
+      // Gram-Schmitt
+      Eigen::Quaterniond current_orientation =
+          mav_msgs::quaternionFromMsg(pose_msg_transformed.pose.orientation);
+      Eigen::Matrix3d current_object_frame =
+          current_orientation.toRotationMatrix();
+      Eigen::Vector3d world_unit_vector_z = {0.0, 0.0, 1.0};
+      Eigen::Vector3d object_unit_vector_y = current_object_frame.col(1);
+      Eigen::Vector3d object_unit_vector_x =
+          object_unit_vector_y.cross(world_unit_vector_z);
+      object_unit_vector_x.normalize();
+      object_unit_vector_y = -object_unit_vector_x.cross(world_unit_vector_z);
+      object_unit_vector_y.normalize();
+      Eigen::Matrix3d current_object_frame_aligned;
+      current_object_frame_aligned << object_unit_vector_x,
+          object_unit_vector_y, world_unit_vector_z;
+      Eigen::Quaterniond current_orientation_aligned(
+          current_object_frame_aligned);
 
+      double current_yaw =
+          mav_msgs::yawFromQuaternion(current_orientation_aligned);
+
+      // initialize filter
+      if (!filter_initialized_) {
+        ROS_INFO("[%s] Initialize filter", ros::this_node::getName().c_str());
+        filtered_position_ = current_position;
+        filtered_yaw_ = current_yaw;
+        filter_initialized_ = true;
+      }
+
+      // filter
+      Eigen::Vector3d delta_position = current_position - filtered_position_;
+      if (delta_position.norm() < filter_parameters_.thresholds.position) {
+        filtered_position_ = position_coeff * current_position +
+                             (1.0f - position_coeff) * filtered_position_;
+      }
+
+      double delta_yaw = current_yaw - filtered_yaw_;
+      if (std::abs(delta_yaw) < filter_parameters_.thresholds.orientation) {
+        filtered_yaw_ = orientation_coeff * current_yaw +
+                        (1.0f - orientation_coeff) * filtered_yaw_;
+      }
+
+      // Publish ros message
       geometry_msgs::PoseWithCovarianceStamped pose_msg_filtered;
       pose_msg_filtered.header = pose_msg_transformed.header;
       pose_msg_filtered.pose.pose.position.x = filtered_position_.x();
@@ -89,7 +157,8 @@ class ObjectPoseFilter {
   ros::Time previous_timestamp_;
   Eigen::Vector3d filtered_position_;
   double filtered_yaw_;
-  float filter_factor_;
+  bool filter_initialized_;
+  FilterParameters filter_parameters_;
 };
 
 int main(int argc, char **argv) {
